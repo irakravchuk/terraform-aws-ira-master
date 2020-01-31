995d8ccf8c214927a8d1255f91ff0b06fc77aff7
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2020-01-31 14:07:25 +0200

    Create version.tf

---
+++ version.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 0.12.10"
+
+  required_providers {
+    aws        = ">= 2.44.0"
+    local      = ">= 1.2"
+    null       = ">= 2.1"
+   
+    random     = ">= 2.1"
+   
+  }
+}
