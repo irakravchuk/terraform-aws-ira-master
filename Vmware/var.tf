eb051c8ab3461d8ea432e10789d66f8e2d26e9fd
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2020-02-17 15:47:01 +0200

    Create var.tf

---
+++ Vmware/var.tf
@@ -0,0 +1,12 @@
+variable "user" {
+  type = string
+  default = "kira"
+}
+variable "password" {
+  type = string
+  default = "kirakira"
+}
+variable "server" {
+  type = string
+  default = "VSPHERE_SERVER"
+}
