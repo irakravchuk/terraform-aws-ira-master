e24c2ca2bfb6977f6dfa721372026ef639710c62
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2020-02-18 10:39:59 +0200

    Update main.tf

--- Vmware/main.tf
+++ Vmware/main.tf
@@ -1,61 +1,15 @@
-#provider "vsphere" {
- # allow_unverified_ssl = true
-#}
-
-#data "vsphere_datacenter" "dc" {
- # name = "DC1"
-#}
-
-#resource "vsphere_folder" "folder" {
- # path = "Scalr Sandbox/test_folder1"
-  #type = "vm"
-  #datacenter_id = data.vsphere_datacenter.dc.id
-#}
-
-
 provider "vsphere" {
-  user           = var.vsphere_user
-  password       = var.vsphere_password
-  vsphere_server = var.vsphere_server
-
-  # If you have a self-signed cert
   allow_unverified_ssl = true
 }
 
 data "vsphere_datacenter" "dc" {
-  name = "dc1"
+  name = "DC1"
 }
 
-data "vsphere_datastore" "datastore" {
-  name          = "datastore1"
+resource "vsphere_folder" "folder" {
+  path = "Scalr Sandbox/test_folder1"
+  type = "vm"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_resource_pool" "pool" {
-  name          = "cluster1/Resources"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
 
-data "vsphere_network" "network" {
-  name          = "public"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-resource "vsphere_virtual_machine" "vm" {
-  name             = "terraform-test"
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
-
-  num_cpus = 2
-  memory   = 1024
-  guest_id = "other3xLinux64Guest"
-
-  network_interface {
-    network_id = data.vsphere_network.network.id
-  }
-
-  disk {
-    label = "disk0"
-    size  = 20
-  }
-}
