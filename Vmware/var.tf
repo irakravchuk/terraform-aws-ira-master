bfb3204baec5a6c6dba797d4a61e4bac616cd1bc
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2020-02-18 10:40:43 +0200

    Update var.tf

--- Vmware/var.tf
+++ Vmware/var.tf
@@ -1,12 +1,12 @@
-variable "vsphere_user" {
-  type = string
-  default = "kira"
-}
-variable "vsphere_password" {
-  type = string
-  default = "kirakira"
-}
-variable "vsphere_server" {
-  type = string
-  default = "VirtualCenter"
-}
+#variable "vsphere_user" {
+ # type = string
+  #default = "kira"
+#}
+#variable "vsphere_password" {
+ # type = string
+  #default = "kirakira"
+#}
+#variable "vsphere_server" {
+ # type = string
+  #default = "VirtualCenter"
+#}
